7cedd39c43a7ffe705fbb33c47efd0b88177909f
javipopotter <javiervalsecaparra@gmail.com>
2021-06-13 22:53:44 +0200

    I forgot setting a lifetime to the bullet in the last update. And i forgot saying that object pooling has been implemented :|

--- Assets/Scripts/ObjectScripts/Gun/Bullet.cs
+++ Assets/Scripts/ObjectScripts/Gun/Bullet.cs
@@ -7,9 +7,21 @@ public class Bullet : MonoBehaviour
     Rigidbody2D rb;
     [HideInInspector] float DmgMultiplier = 1f;
     public bool IsPickedByEnemy;
+    float lifetime = 3;
+
+    private void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if(lifetime <= 0)
+        {
+            lifetime = 3;
+            gameObject.SetActive(false);
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        lifetime = 3;
         rb = GetComponent<Rigidbody2D>();
         GameManager.Gm.StartCoroutine(GameManager.Gm.DoDamage(collision, rb, DmgMultiplier, IsPickedByEnemy));
         GameManager.Gm.UpdateColliders(GetComponent<Collider2D>(), false, GameManager.Gm.AlliesColliders);
